cb069429930193f685d0470bfcf62626470f8460
putrieltarie <putrilestari.eltarie@gmail.com>
2025-06-24 22:34:07 +0700

    import data & buat model reglog

--- validation.R
+++ validation.R
@@ -2,3 +2,13 @@
 library(ggplot2)
 library(dplyr)
 library(readr)
+
+#Import data
+credit_scoring <- read.csv("D:/WORK/REMOTE/ds-takehome/data/credit_scoring.csv")
+data_credit_copy = credit_scoring
+
+#Membuat model regresi logistik
+library(glmtoolbox)
+model <- glm(default ~ age + monthly_income + loan_amount + previous_defaults + credit_score, data = credit_scoring, family = binomial())
+summary(model)
+
